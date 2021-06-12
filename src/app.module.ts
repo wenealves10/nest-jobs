1809d7129ff9f32908b729235b0ea149d0bb7c67
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-12 12:34:19 -0300

    Created new Modules and Configuration Redis 🥉

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CreateUserController } from './create-user/create-user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT, 10),
+        password: process.env.REDIS_PASSWORD,
+      },
+    }),
+  ],
+  controllers: [CreateUserController],
+  providers: [],
 })
 export class AppModule {}
